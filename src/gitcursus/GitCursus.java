47d688c931ce5d77573fd7119633bd81cde1b70f
lorienvanderoy <lorienvanderoy@gmail.com>
2015-11-26 09:51:57 +0100

    Update GitCursus.java

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -15,7 +15,7 @@ public class GitCursus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("bonjour");
+        System.out.println("iedereen bonjour");
     }
     
 }
